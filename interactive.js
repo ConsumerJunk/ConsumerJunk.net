be78b51f39b665191cb3c3aa93e4b536c55d6232
ConsumerJunk <cj.simmons.email@gmail.com>
2019-08-09 13:04:25 -0700

    Update

--- interactive.js
+++ interactive.js
@@ -1,17 +1,44 @@
 var menu_open = false;
+var content_accessed = false;
 
 $("document").ready(function() {
 	$('img').on('dragstart', function(event) {
 		event.preventDefault();
 	});
 	$("#menu_toggle").click(function() {
-		menu_open = !menu_open;
 		if(menu_open) {
-			$("#menu").removeClass();
-			$("#menu").addClass("open");
+			close_menu();
 		} else {
-			$("#menu").removeClass();
-			$("#menu").addClass("closed");		
+			open_menu();
 		}
 	});
+	$("#menu").mouseleave(function() {
+		if(content_accessed) {
+			console.log("CLOSING");
+			close_menu();
+			content_accessed = false;
+		}
+	});
+	$("[goto]").click(function(event) {
+		event.preventDefault();
+		$(".content").removeClass("shown_content");
+		$(".content").addClass("hidden_content");
+		var goto = $(event.target).attr("goto");
+		$("#" + goto).removeClass("hidden_content");
+		$("#" + goto).addClass("shown_content");
+		content_accessed = true;
+	});
 });
+
+var close_menu = function() {
+	$("#menu").removeClass();
+	$("#menu").addClass("closed");
+	menu_open = false;
+	content_accessed = false;
+}
+
+var open_menu = function() {
+	$("#menu").removeClass();
+	$("#menu").addClass("open");
+	menu_open = true;
+}
