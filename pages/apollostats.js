b5963aadcf78b06cedf3701593788c906937a73b
ConsumerJunk <cj.simmons.email@gmail.com>
2019-08-09 17:52:03 -0700

    Bypassed CORS

--- pages/apollostats.js
+++ pages/apollostats.js
@@ -1,4 +1,4 @@
 var get = function() {
-	var url = "https://bitbucket.org/ConsumerJunk/apollostatsrepo/raw/9d1c6f205fa00cf34358658207de09766a15a719/test.txt";
+	var url = "https://crossorigin.me/https://bitbucket.org/ConsumerJunk/apollostatsrepo/raw/9d1c6f205fa00cf34358658207de09766a15a719/test.txt";
 	$("#response").load(url);
 }
